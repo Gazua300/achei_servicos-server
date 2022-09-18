f6b0643c67de7813b905a95d0bbe94fd4f7676eb
Gazua300 <mesmo245@gmail.com>
2022-09-18 00:31:23 -0300

    mudanças para alterações

--- src/endpoints/deleteJob.ts
+++ src/endpoints/deleteJob.ts
@@ -21,7 +21,11 @@ export const deleteJob = async(req:Request, res:Response):Promise<void>=>{
             provider: job.provider
         })
 
-        if(provider){
+        const [jobId] = await con('labeninja_contratado').where({
+            job_id: job.id
+        })
+
+        if(provider && jobId){
             statusCode = 403
             throw new Error('Você não pode excluir este serviço pois ele já foi contratado. Entre em contato com o cliente para cancelá-lo se quiser excluí-lo!')
         }
