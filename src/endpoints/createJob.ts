463b93135bffb328b30863fe337e1509c164a99e
Gazua300 <mesmo245@gmail.com>
2022-10-04 16:42:59 -0300

    atualizações em andamento

--- src/endpoints/createJob.ts
+++ src/endpoints/createJob.ts
@@ -7,7 +7,7 @@ export const createJob = async(req:Request, res:Response):Promise<void>=>{
   let statusCode = 400
   try{
 
-    const { title, description, phone, period, provider } = req.body
+    const { title, description, phone, period } = req.body
 
 
     if(!title || !description || !phone || !period){
@@ -35,8 +35,7 @@ export const createJob = async(req:Request, res:Response):Promise<void>=>{
       title,
       description,
       phone,
-      period,
-      provider
+      period
     })
     
 
