e750ff487b19ced6db75ebda5dfc129c88e1fb2e
Gazua300 <mesmo245@gmail.com>
2023-03-22 04:21:51 -0300

    adicionando endpoint para upload de imagens com multer

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,37 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
-
-
-import { getAllJobs } from './endpoints/getAllJobs'
-import { createJob } from   './endpoints/createJob'
-import { getJobById } from './endpoints/getJobById'
-import { updatePushToken } from './endpoints/updatePushToken'
-import { deleteJob } from './endpoints/deleteJob'
-
+import multer from 'multer'
 
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb)=>{
+      cb(null, './src/uploads')
+  },
+  filename: (req, file, cb)=>{
+      cb(null, `${new Date().getTime().toString(36)}_${file.originalname}`)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+
+import { getAllJobs } from './endpoints/getAllJobs'
+import { createJob } from   './endpoints/createJob'
+import { uploadJobImage } from './endpoints/uploadJobImage'
+import { getJobById } from './endpoints/getJobById'
+import { updatePushToken } from './endpoints/updatePushToken'
+import { deleteJob } from './endpoints/deleteJob'
+
+
 app.get('/jobs', getAllJobs)
 app.get('/job/:id', getJobById)
 app.post('/jobs', createJob)
+app.post('/image', upload.single('image'), uploadJobImage)
 app.put('/pushtoken/:id', updatePushToken)
 app.delete('/job/:id', deleteJob)
 
@@ -24,5 +39,5 @@ app.delete('/job/:id', deleteJob)
 
 
 app.listen(process.env.PORT || 3003, ()=>{
-  console.log('Server runing at http://localhost:3003')
+  console.log('Server running at http://localhost:3003')
 })
